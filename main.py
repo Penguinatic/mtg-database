508dfe86178377ca65317c3c6c3d54026dc4061d
Alfie Richardson <01richardsonalfie@gmail.com>
2021-02-26 20:55:36 +0000

    Added all the data I could think of storing for a card, current error about how many columns there are vs how many are expected

--- main.py
+++ main.py
@@ -5,7 +5,16 @@ active = True
 conn = sqlite3.connect("cards.db")
 cur = conn.cursor()
 
-# cur.execute("CREATE TABLE cards (name)")
+cur.execute("CREATE TABLE IF NOT EXISTS "
+            "cards (name text, "
+            "setName text, "
+            "setCode text, "
+            "setNum integer, "
+            "manaCost text, "
+            "cmc integer"
+            "cardText text, "
+            "flavourText text, "
+            "quantity integer)")
 
 while active:
     userInput = input("\nPlease enter a card name to add, 'quit' or 'all':")
@@ -15,6 +24,15 @@ while active:
         cur.execute("SELECT * FROM cards")
         rows = cur.fetchall()
         for row in rows:
-            print("Name: " + row[0])
+            print("Card Name: " + row[0] + " --- "
+                  "Set Name: " + row[1] + " --- "
+                  "Set Code: " + row[2] + " --- "
+                  "Number in Set: " + str(row[3]) + " --- "
+                  "Mana Cost: " + row[4] + " --- " 
+                  "Converted Mana Cost: " + str(row[5]) + " --- "
+                  "Card Text: " + row[6] + " --- "
+                  "Flavour Text: " + row[7] + " --- " +
+                  "Quantity: " + row[8])
     else:
-        cur.execute("INSERT INTO cards VALUES (?)", [userInput])
+        cur.execute("INSERT INTO cards VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                    (userInput, "n", "code", 0, "cost", 0, "c", "f", 0))
